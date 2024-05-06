ef7b79238d7ce1be2c9d571344fd776b7793a99a
SithumRaigamage <sraig2002@gmail.com>
2024-05-06 17:09:54 +0530

    Initial commit

---
+++ tutorials.js
@@ -0,0 +1,39 @@
+//running an example
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello JavaScript";
+}
+//display in varoius ways
+
+//document.getElementById("text").innerHTML =5+6;
+//document.write(5+6);
+//window.alert(5+6);
+//alert(11);
+//console.log(11)
+
+// java script statements
+let x,y,z ;
+x=5;
+y=6;
+z=x+y;
+document.getElementById("textone").innerHTML = "Heloo Dolly";
+
+let person = "Hege";
+//prints out error if we make the same name
+//let person ="Hege";
+
+function functionOne(){
+    document.getElementById("textone").innerHTML = "Hello Dolly";
+    document.getElementById("texttwo").innerHTML = "How are You ?";
+}
+//javaScript keywords
+/*
+var defines the variable
+let defines the bloc
+const defines the constant
+if for condition
+switch multiple if statements
+for loop for iterating
+function to make a method
+return exits a function
+try for file handling
+*/

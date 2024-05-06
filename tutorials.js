dedf5ddc59c75d58d782a6e389174a6401c97a55
SithumRaigamage <sraig2002@gmail.com>
2024-05-06 17:50:45 +0530

    basics done

--- tutorials.js
+++ tutorials.js
@@ -11,10 +11,12 @@ function myFunction() {
 //console.log(11)
 
 // java script statements
+/*
 let x,y,z ;
 x=5;
 y=6;
 z=x+y;
+*/
 document.getElementById("textone").innerHTML = "Heloo Dolly";
 
 let person = "Hege";
@@ -37,3 +39,81 @@ function to make a method
 return exits a function
 try for file handling
 */
+
+
+//javaScript syntax
+
+//var b;
+//let a;
+
+a=7;
+b=8;
+//let c=a+b;
+
+//js literals 
+
+10.50
+1001
+
+"Hello John Doe"
+
+let sum =(5+6*10);
+
+"John" + " " + "Doe"
+
+//creating variables with var
+var d=6;
+var e=4;
+
+let lastname, lastName;
+lastName = "Doe";
+lastname = "Peterson";
+
+// js comments
+
+//single line comments ==//
+
+//multiline comments in js
+
+/*
+document.getElementById("myH").innerHTML = "My First Page";
+document.getElementById("myP").innerHTML = "My first paragraph.";
+*/
+
+//js variables
+
+// we can use var, let, and const to create variables
+
+// Using simple assignment
+let x = 5;
+let y = 6;
+let z = x + y;
+document.getElementById("demo1").innerHTML = "The value of z is: " + z;
+
+// Using var
+var a = 5;
+var b = 6;
+var c = a + b;
+document.getElementById("demo2").innerHTML = "The value of c is: " + c;
+
+// Using const
+const p = 5;
+const q = 6;
+const r = p + q;
+document.getElementById("demo3").innerHTML = "The value of r is: " + r;
+
+// Using let
+let m = 5;
+let n = 6;
+let o = m + n;
+document.getElementById("demo4").innerHTML = "The value of o is: " + o;
+
+//mixed example 
+
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+document.getElementById("demo1").innerHTML = "The value of total is: " + total;
+
+
+

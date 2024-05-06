c482fa6a9cd3250641436cb2ef16ab6672d8953a
SithumRaigamage <sraig2002@gmail.com>
2024-05-06 18:37:55 +0530

    added another functionalities

--- tutorials.js
+++ tutorials.js
@@ -116,4 +116,92 @@ let total = price1 + price2;
 document.getElementById("demo1").innerHTML = "The value of total is: " + total;
 
 
+//variables global and local
 
+ //let - local scope and var is global scope
+
+
+ //const - assigning a variable that cnnot be changed
+ const PI = 3.14159265359;
+
+ // operators
+
+/*
++	Addition
+-	Subtraction
+*	Multiplication
+**	Exponentiation (ES2016)
+/	Division
+%	Modulus (Division Remainder)
+++	Increment
+--	Decrement
+==    Equal to
+*/
+
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
+
+let text3 = "John";
+let text4 = "Doe";
+let text5 = text1 + " " + text2;
+
+//functions
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello Dolly";
+}
+
+//calling a function
+myFunction();
+
+function myFunctionOne(p1,p2) {
+    let sum = p1+p2;
+    return sum;
+    
+}
+
+var sumOne = myFunctionOne(4,3);
+console.log(x);
+
+function toCelsuis(farenheit){
+    return (5/9) * (farenheit - 32);
+}
+let value1 = toCelsuis();
+
+let value = toCelsius(77);
+let text = "The temperature is " + x + " Celsius";
+
+//objects
+
+let personTwo = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+};
+personTwo.fullName;
+personTwo.age;
+
+//objects can be let ,var and const they are in a list {} with variables and attributes
+
+//js events
+
+/*
+
+onchange -	An HTML element has been changed
+onclick -	The user clicks an HTML element
+onmouseover -	The user moves the mouse over an HTML element
+onmouseout -	The user moves the mouse away from an HTML element
+onkeydown -	The user pushes a keyboard key
+onload -	The browser has finished loading the page
+
+*/
+
+//js strings
+
+let text9 = "Hello Dolly";
+let result2 = text.length;
+
+let textr = "Hello Dolly";
+let result8 = text.toUpperCase();

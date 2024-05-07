85258254dd4d42bedbdffcb19df2f6bda5b6a578
SithumRaigamage <sraig2002@gmail.com>
2024-05-07 22:19:56 +0530

    Js more tutorials did

--- tutorials.js
+++ tutorials.js
@@ -1,4 +1,5 @@
 //running an example
+/*
 function myFunction() {
     document.getElementById("demo").innerHTML = "Hello JavaScript";
 }
@@ -16,7 +17,7 @@ let x,y,z ;
 x=5;
 y=6;
 z=x+y;
-*/
+
 document.getElementById("textone").innerHTML = "Heloo Dolly";
 
 let person = "Hege";
@@ -38,7 +39,7 @@ for loop for iterating
 function to make a method
 return exits a function
 try for file handling
-*/
+
 
 
 //javaScript syntax
@@ -78,7 +79,7 @@ lastname = "Peterson";
 /*
 document.getElementById("myH").innerHTML = "My First Page";
 document.getElementById("myP").innerHTML = "My first paragraph.";
-*/
+
 
 //js variables
 
@@ -136,7 +137,7 @@ document.getElementById("demo1").innerHTML = "The value of total is: " + total;
 ++	Increment
 --	Decrement
 ==    Equal to
-*/
+
 
 let text1 = "A";
 let text2 = "B";
@@ -196,7 +197,7 @@ onmouseout -	The user moves the mouse away from an HTML element
 onkeydown -	The user pushes a keyboard key
 onload -	The browser has finished loading the page
 
-*/
+
 
 //js strings
 
@@ -205,3 +206,139 @@ let result2 = text.length;
 
 let textr = "Hello Dolly";
 let result8 = text.toUpperCase();
+*/
+
+/*
+//array
+// Create an Array:
+var cars =["BMW","Toyota","Honda"]
+
+// Change an element:
+cars[0] = "Audi";
+
+// Add an element:
+cars.push("BMW");
+
+cars.pop("BMW")
+
+// Display the Array:
+document.getElementById("array").innerHTML = cars; 
+*/
+/*
+// You can create a const object:
+const car ={Model:"Honda",color:"yellow",owner:"Sithum"}
+
+// You can change a property:
+car.color = "red";
+
+// You can add a property:
+car.owner = "Johnson";
+
+document.getElementById("array").innerHTML = car.owner; 
+
+//comparison operators
+
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
+document.getElementById("demo").innerHTML = "Is A less than B? " + result;
+
+
+let variable ="String";
+let variables;
+document.getElementById("demo").innerHTML = 'This is "Sithum"';
+
+if(condition){
+    var condition=true;
+    console.log(condition)
+}
+if (hour < 18) {
+    greeting = "Good day";
+  } else {
+    greeting = "Good evening";
+  }
+
+  if(hour>18){
+    greeting="good night"
+  }
+  else{
+    greeting=" good morning"
+  }
+
+*/
+  //switch case
+
+  switch(condition){
+    case  "apple":
+        break;
+    case "grapes":
+        break;
+    default:
+    
+  }
+let day;
+  switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+    
+}
+document.getElementById("day").innerHTML = "Today is " + day;
+
+//for loop
+for (i=0;i<9;i++){
+    console.log("I am running for the "+(i+1)+" time");
+}
+
+//while loop
+while(i<9){
+    console.log("Value of i is "+i);
+    i++;
+}
+
+//for exception handling use try and catch
+try {
+    //Block of code to try
+}
+catch(err) {
+    //Block of code to handle errors
+}
+finally {
+    //Block of code to be executed regardless of the try / catch result
+}
+
+function myFunction() {
+    const message = document.getElementById("p01");
+    message.innerHTML = "";
+    let x = document.getElementById("demo").value;
+    try {
+      if(x.trim() == "") throw "is empty";
+      if(isNaN(x)) throw "is not a number";
+      x = Number(x);
+      if(x > 10) throw "is too high";
+      if(x < 5) throw "is too low";
+    }
+    catch(err) {
+      message.innerHTML = "Error: " + err + ".";
+    }
+    finally {
+      document.getElementById("demo").value = "";
+    }
+}
